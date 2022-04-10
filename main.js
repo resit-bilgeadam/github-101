96f22c829cb46363e125e8e61caab92a7d80759a
Yasin Özgür Çakmak <cakmakyasinozgur@gmail.com>
2022-04-10 11:02:06 +0300

    change backgorund

--- main.js
+++ main.js
@@ -1,7 +1,12 @@
 const btn = document.querySelector("#btn");
+const changeBackground = document.querySelector("#changeBackground");
 
 function handleClick() {
   alert("Clicked Button!!");
 }
+function changeBackgroundHandler(){
+  document.querySelector('body').style.background = 'grey'
+}
 
 btn.addEventListener("click", handleClick);
+changeBackground.addEventListener('click',changeBackgroundHandler)
